678f8197513784afdbfa1c527ed61b88fa253ca7
belbrun <brunobelic@hotmail.com>
2019-03-24 19:32:09 +0100

    Implement forward propagation of convolution layers

--- layers.py
+++ layers.py
@@ -18,11 +18,13 @@ class Layer:
 class ConvolutionLayer(Layer):
 
 
-    def __init__(self, filterNumber, filterSize, stride):
+    def __init__(self, filterNumber, filterSize, stride, activationFunction):
         self.filters = []
+        self.bias = 0
         for i in range(0, filterNumber):
             self.filters.append(np.matrix(np.array((filterSize,filterSize))))
         self.stride = stride
+        self.activationFunction = activationFunction
 
     @static
     def generateConvolutionLayer():
@@ -30,8 +32,18 @@ class ConvolutionLayer(Layer):
         Returns a new convolution layer given the input data parameters.
     """
 
-    def propagateForward(self, x):
-        pass
+    def propagateForward(self, input):
+        outputShape = input[0].shape[0] - self.filterSize + 1
+        y = np.matrix(np.array(outputShape,outputShape))
+        for i in range(0, x.shape[0] - self.filterSize):
+            for j in range(0, x.shape[1] - self.filterSize):
+                for a in range(0, self.filterSize):
+                    for b in range(0, self.filterSize):
+                        for (d,x) in enumerate(input):
+                            y[i,j] += self.filters[d][a,b] * \
+                            x[i*self.stride + a, j*self.stride + b] + self.bias)
+        return self.activationFunction(y)
+
 
     def propagateBackwards(self, error):
-        pass
+        
