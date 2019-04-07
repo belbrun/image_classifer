ac850af6f95144f491112449a76402943060d50d
belbrun <bruno.belic@fer.hr>
2019-04-08 00:23:30 +0200

    Implement neural network class (WIP)

--- network.py
+++ network.py
@@ -2,5 +2,70 @@
 class NeuralNetwork():
 
     def __init__(self):
-
         self.layers = []
+
+
+def addLayer(self, layer):
+    """
+        Add a layer to the neural network in sequential fashion.
+    """
+    self.layers.append(layer)
+
+def output(self, x):
+    """
+        Calculate the output for a single input instance x (one row from
+        the training or test set)
+    """
+
+    # For each layer - calcuate the output of that layer and use it
+    # as input to the following layer. The method should return the
+    # output of the last layer. The input to the first layer is the
+    # vector x.
+
+    for layer in self.layers:
+        x = layer.propagateForward(x)
+
+    return x
+
+def calculateOutputs(self, data):
+
+    outputs = []
+    for instance in data:
+        outputs.append(self.output(instance))
+
+    return outputs
+
+
+def feed(self, data):
+
+    outputs = self.calculateOutputs(data)
+    return outputs
+
+
+def calculateError(self, output, result):
+
+    errors = []
+    return errors.append(self.errorFunction(output, result))
+
+def getOverallError(self, outputs, results):
+
+    overallError = 0
+    for i in range(0, len(outputs)):
+        for error in self.calculateError(outputs[i], results[i])
+            overallError += error #absoulte ?
+
+    return error
+
+def learn(errors):
+
+    for layer in self.layers[::-1]:
+        errors = layer.propagateBackwards(errors)
+
+
+def train(self, data, results, learningRate):
+
+    for i in range(0, len(data)):
+
+        output = self.output(data[i])
+        errors = self.calculateError(output, results[i])
+        self.learn()
