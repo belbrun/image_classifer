933410264e16d85561b699ffe9da1db938d5adf7
belbrun <bruno.belic@fer.hr>
2019-09-25 23:42:33 +0200

    Remove modules that are made specifically for blastom classifier

--- datautil.py
+++ datautil.py
@@ -11,79 +11,14 @@ import parser
 
 
 
-def getInput(index, isBlastom, path = 'dataset/2/ALL_IDB2/img/', gray = False,
-        shape = None, rotations = False, avaraged = False):
 
-    name = parser.generateName(index, isBlastom)
-    imageAsArrays = pp.getImageAsArrays(name, path, gray, shape, rotations)
-    output = []
-
-    for rotation in imageAsArrays:
-        output.append(pp.processArrays(rotation, gray, avaraged))
-
-    return output
-
+def saveData(path, data):
+    with open(path + 'config.txt', 'w+') as file:
+        for information in data:
+            file.write(str(information)+'|')
 
 
 def loadData(path, old = False):
     name = 'old_config.txt' if old else 'config.txt'
     with open(path + name, 'r+') as file:
         return file.read().split('|')
-
-def makeDirectory(path):
-    os.mkdir(path)
-
-def writeLog(path, log):
-    with open(path + 'log.txt', 'w+') as file:
-        for line in log:
-            file.write(line + '\n')
-
-def readLog(path):
-    with open(path + 'log.txt', 'r+') as file:
-        return file.read().split('\n')
-
-def writeResults(path, results):
-    with open(path + 'results.txt', 'w+') as file:
-        for line in results:
-            file.write(str(line) + '|')
-
-def readResults(path):
-    with open(path + 'results.txt', 'r+') as file:
-        return file.read().split('|')
-
-def getLayerIds(path):
-    layerIds = []
-    numOfLayers = len([i for i in os.listdir(path) if not i.endswith('.txt')])
-    for i in range(0, numOfLayers):
-        with open(path + str(i) + '/config.txt', 'r+') as file:
-            layerIds.append(file.read(4))
-    return layerIds
-
-def getValidationResults(path):
-    log = readLog(path)
-    return parser.parseLinesForResults(log, 'VALIDATION RESULTS')
-
-def getTrainingResults(path):
-    log = readLog(path)
-    return parser.parseLinesForResults(log, '[')
-
-def getTestResults(path):
-    resultsString = readResults(path)
-    return  parser.parseLinesForResults(resultsString)
-
-def getEpochValidationOutputs(epochPath, epoch):
-    return parser.parseEpochValidationOutputs(readLog(epochPath), epoch)
-
-
-if __name__ == '__main__':
-    arrays = getInput('Im001_1.tif', gray = True, shape = (200,200), avaraged = True, rotations = True)
-    #arrays = getInput('Im001_1.tif', gray = True)
-    #arrays = getInput('Im001_1.tif', shape = (200,200))
-    #arrays = getInput('Im001_1.tif', rotations = True)
-    #arrays = getInput('Im001_1.tif', avaraged = True)
-    #arrays = getInput('Im001_1.tif')
-
-    for i, array in enumerate(arrays):
-
-        image = getImageFromArrays(array, True)
-        image.save('dataset/examples/' + str(i) + '.png', 'png')
